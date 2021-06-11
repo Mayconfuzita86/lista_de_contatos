28332e77158131feb88752221a83eea5cb899842
mayconfuzita <mayconfuzita@gmail.com>
2021-06-10 18:37:46 -0600

    add png

--- app/src/main/java/com/dioinnovation/listadecontatos/helpers/HelperDB.kt
+++ app/src/main/java/com/dioinnovation/listadecontatos/helpers/HelperDB.kt
@@ -59,6 +59,7 @@ class HelperDB(
 
 //        var where = "$COLUMNS_NOME LIKE ?"
 //        var buscaComPercentual = arrayOf("%$busca%")
+
         var cursor = db.query(TABLE_NAME, null, where, args, null, null, null)
 
         if (cursor == null){
@@ -82,29 +83,25 @@ class HelperDB(
 
         //val sql = "INSERT INTO $TABLE_NAME($COLUMNS_NOME, $COLUMNS_TELEFONE) values (?, ?)"
         //var array = arrayOf(contato.nome, contato.telefone)
+        // db.execSQL(sql, array)
 
         var content = ContentValues()
         content.put(COLUMNS_NOME, contato.nome)
         content.put(COLUMNS_TELEFONE, contato.telefone)
         db.insert(TABLE_NAME, null, content)
-
-        // db.execSQL(sql, array)
-
         db.close()
-
     }
 
     fun deletarContato(id: Int) {
         val db = writableDatabase ?: return
 
-        val sql = "DELETE FROM $TABLE_NAME WHERE $COLUMNS_ID = ?"
-        val arg = arrayOf("$id")
-        db.execSQL(sql, arg)
-
 //        val where = "id = ?"
 //        val arg = arrayOf("$id")
 //        db.delete(TABLE_NAME, where,arg)
 
+        val sql = "DELETE FROM $TABLE_NAME WHERE $COLUMNS_ID = ?"
+        val arg = arrayOf("$id")
+        db.execSQL(sql, arg)
         db.close()
     }
 
